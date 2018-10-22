c6e2eaa1385f0d1da9dec37b9522f9de84c7e274
alfennec <44066991+alfennec@users.noreply.github.com>
2018-10-22 10:43:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bermed/smarttab/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.bermed.smarttab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // commentaire
+
+        TextView tv = (TextView) findViewById(R.id.tv);
+    }
+}
